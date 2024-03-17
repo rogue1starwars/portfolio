421cb2b4f663eb39ecb0cd1b67182bceeb16183b
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-03-17 10:57:53 +0900

    modified head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,13 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `Ryota - Robotics and Web Design`,
+  description: `A portfolio of Ryota's work in robotics and web design.`,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
